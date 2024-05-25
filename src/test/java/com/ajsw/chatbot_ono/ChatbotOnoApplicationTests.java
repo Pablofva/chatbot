13f6b63fd5a3e5d08d9e011100fb540e60d8e8a0
Pablofva <pablovzqz768@gmail.com>
2024-05-24 22:49:50 -0300

    init

---
+++ src/test/java/com/ajsw/chatbot_ono/ChatbotOnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajsw.chatbot_ono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotOnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
