5d5e8160520c70555a1c10ec60db6e439b88b904
Pablofva <pablovzqz768@gmail.com>
2024-05-26 15:47:54 -0300

    Se le agregaron los atributos al chatboy y a travez del orm se crea la tabla chatbot, conversations

--- src/main/java/com/ajsw/chatbot_ono/models/Chatbot.java
+++ src/main/java/com/ajsw/chatbot_ono/models/Chatbot.java
@@ -1,4 +1,26 @@
 package com.ajsw.chatbot_ono.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name="chatbot_conversations")
 public class Chatbot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String userInput;
+    private String botResponse;
+    private LocalDateTime timestamp;
 }
